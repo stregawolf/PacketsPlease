63de2065ba68d26eb77d21b5b8e2c15ea22e026d
Ted <rebel.esd@gmail.com>
2018-01-26 23:56:40 -0800

    NameGen with First and last names
    Adds a Name struct with quick "First Last" and "Last, First" prints; generated from a list of most popular baby names for the last hundred + years and a registry of last names.

--- Assets/PacketsPlease/Scripts/Data/CustomerData.cs
+++ Assets/PacketsPlease/Scripts/Data/CustomerData.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "CustomerData", menuName = "Data/CustomerData", order = 1)]
 public class CustomerData : ScriptableObject {
-    public string m_name;
+    public NameGen.Name m_name;
     public float m_dataUsage;
+    public bool m_male = false;
 
     public void Generate()
     {
-        m_name = "McPoopyPants";
+        m_male = Random.value > 0.5f;
+        m_name = NameGen.GetName(m_male);
         m_dataUsage = Random.Range(0.0f, 100.0f);
     }
 }
