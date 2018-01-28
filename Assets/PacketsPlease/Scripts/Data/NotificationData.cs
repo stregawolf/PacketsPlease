ee87e2c684b248cbac86db0d22059aea3202ed47
stregawolf <stregawolf@gmail.com>
2018-01-28 13:20:07 -0800

    added body assets

--- Assets/PacketsPlease/Scripts/Data/NotificationData.cs
+++ Assets/PacketsPlease/Scripts/Data/NotificationData.cs
@@ -88,7 +88,7 @@ public class NotificationData : ScriptableObject
     {
         m_title = "You're FIRED!";
         m_sender = SENDER_BOSS;
-        m_message = "You're not cut out to work at this company.\n\nYou're FIRE!";
+        m_message = "That's the last strike!\n\nYou're not cut out to work at this company.\n\nYOU ARE FIRE!!!";
         m_response = null;
         m_iconColor = Color.red;
         m_correctResponseAction = ResolutionAction.GameOver;
