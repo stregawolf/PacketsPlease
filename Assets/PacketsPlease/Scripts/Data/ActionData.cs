81e221637efac94f27a29e1779a80bb2de6203f9
Jordan Cazamias <jacazamias@gmail.com>
2018-01-27 16:36:34 -0800

    RuleManager: passed/violated rules will compare priorities

--- Assets/PacketsPlease/Scripts/Data/ActionData.cs
+++ Assets/PacketsPlease/Scripts/Data/ActionData.cs
@@ -9,16 +9,39 @@ public enum ActionType
     Disconnect
 }
 
-// public class ActionData : ScriptableObject {
+public class ActionData : ScriptableObject {
 
-//     public CustomerData customer;
+    public CustomerData customer;
+    public ActionType actionType;
 
-//     public ActionType actionType;
+    // Passed/Violated rules lists, in descending order of priority
+    // TODO: Make this SortedList
+    public List<RuleData> passedRules;
+    public List<RuleData> violatedRules;
 
-//     public ActionData(CustomerData customer, ActionType actionType)
-//     {
-//         this.customer = customer;
-//         this.actionType = actionType;
-//     }
+    public RuleData HighestViolatedRule { get { return (violatedRules.Count == 0) ? null : violatedRules[0]; } }
+    public RuleData HighestPassedRule { get { return (passedRules.Count == 0) ? null : passedRules[0]; } }
 
-// }
+    public ActionData(CustomerData customer, ActionType actionType)
+    {
+        this.customer = customer;
+        this.actionType = actionType;
+
+        passedRules = new List<RuleData>();
+        violatedRules = new List<RuleData>();
+    }
+
+    public void PrintAllRules()
+    {
+        Debug.Log("PASSED RULES");
+        for(int i = 0; i < a.passedRules.Count; i++)
+        {
+            Debug.Log(passedRules[i].ToString());
+        }
+        Debug.Log("VIOLATED RULES");
+        for(int i = 0; i < a.violatedRules.Count; i++)
+        {
+            Debug.Log(violatedRules[i].ToString());
+        }
+    }
+}
--- Assets/PacketsPlease/Scripts/Managers/RuleManager.cs
+++ Assets/PacketsPlease/Scripts/Managers/RuleManager.cs
@@ -24,41 +24,68 @@ public class RuleManager : Singleton<RuleManager> {
         m_rules.Add(rule);
     }
 
-    // Check action against all rules in place.
-    // Return highest priority rule that's violated, if any.
-    // Return null if no rules are violated.
-    public RuleData GetHighestViolatedRule(CustomerData customer, ActionType actionTaken)
-    {
-        List<RuleData> violatedRules = GetViolatedRules(customer, actionTaken);
 
-        if(violatedRules == null)
-        {
-            return null;
-        }
-
-        return violatedRules[0];
-    }
-
-    public List<RuleData> GetViolatedRules(CustomerData customer, ActionType actionTaken)
+    // Determine whether the rules have been violated by an action`
+    // Also populates actionTaken with sorted list of passed and violated rules
+    public bool DoesViolateRules(ActionData actionTaken)
     {
         var violatedRules = new List<RuleData>();
+        var passedRules = new List<RuleData>();
 
         for(int i = 0; i < m_rules.Count; i++)
         {
             RuleData rule = m_rules[i];
+
+            // Check that rule applies and is violated
+            if(!rule.DoesApply(actionTaken))
+            {
+                continue;
+            }
+
             if(rule.IsViolated(customer, actionTaken))
             {
                 violatedRules.Add(rule);
             }
-        }
-
-        if(violatedRules.Count == 0)
-        {
-            return null;
+            else
+            {
+                passedRules.Add(rule);
+            }
         }
 
         violatedRules.OrderByDescending((rule) => rule.m_priority);
+        passedRules.OrderByDescending((rule) => rule.m_priority);
 
-        return violatedRules;
+        actionTaken.violatedRules = violatedRules;
+        actionTaken.passedRules = passedRules;
+
+        // Check if violated rules or passed rules take priority
+        if(violatedRules.Count == 0)
+        {
+            return false;
+        }
+        else if(passedRules.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            int vp = violatedRules[0].m_priority;
+            int pp = passedRules[0].m_priority;
+
+            if(vp == pp) 
+            {
+                DumpRulesAndCrash(actionTaken, "RuleManager: Violated and Passed rules have equal priority");
+            }
+            else
+            {
+                return (violatedRules[0].m_priority > passedRules[0].m_priority);
+            }
+        } 
+    }
+
+    private void DumpRulesAndCrash(ActionData a, string msg)
+    {
+        a.PrintAllRules();
+        throw new Exception(msg);
     }
 }
